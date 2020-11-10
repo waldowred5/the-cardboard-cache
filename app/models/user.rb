7226084b4d6e8cf6434bbfb8bc5c417fadaee349
waldowred5 <dwaldow@me.com>
2020-11-10 13:27:44 +1100

    Added associations between all tables

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one :address
+  has_many :owned_games
+  has_many :wished_games
 end
--- app/models/wished_game.rb
+++ app/models/wished_game.rb
@@ -1,2 +1,4 @@
 class WishedGame < ApplicationRecord
+    belongs_to :wishlister, class_name: "User"
+    belongs_to :board_game
 end
