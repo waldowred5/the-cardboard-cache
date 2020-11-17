06fa7a6d4c3e4d8e22d3c51ca0252ed649a7fd9e
waldowred5 <dwaldow@me.com>
2020-11-17 16:11:49 +1100

    Fixed precompile config settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  # Stop assets trying to initialize_on_precompile
+  config.assets.initialize_on_precompile = false
 end
